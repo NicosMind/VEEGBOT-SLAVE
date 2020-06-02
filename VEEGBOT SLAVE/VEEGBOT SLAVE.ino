2c7030fdb626058902f2125e12c4e149a2bcd90d
NicosMind <nico_drift@hotmail.fr>
2020-06-02 11:21:21 +0200

    preparation for serial command

--- VEEGBOT SLAVE/VEEGBOT SLAVE.ino
+++ VEEGBOT SLAVE/VEEGBOT SLAVE.ino
@@ -5,10 +5,7 @@
 */
 
 
-volatile long temp, counter = 0; //This variable will increase or decrease depending on the rotation of encoder
 
-#define  A_PHASE 21
-#define  B_PHASE 22
 
 int PUMP_ONOFF, VACUUM_PUMP_ONOFF;
 
@@ -17,31 +14,24 @@ int MAX_Z_STEPS, MAX_R_STEPS, MAX_T_STEPS;
 
 int Z_ENABLE, Z_DIR, Z_STEP, Z_MIN, Z_MAX, Z_COUNT, Z_SPEED;
 int R_ENABLE, R_DIR, R_STEP, R_MIN, R_MAX, R_COUNT, R_SPEED;
-int TETA_ENABLE, TETA_DIR, TETA_STEP, TETA_MIN, TETA_MAX, TETA_SPEED, T_COUNT;
-int TETA_MAXSTEP = 436320;
+
 
 const int CW = HIGH;
 const int CCW = LOW;
 
-bool enableStateTETA = false;
+
 bool enableStateR = false;
 bool enableStateZ = false;
 
-int countingStepsTETA = 0;
+
 int countingStepsR = 0;
 int countingStepsZ = 0;
 
 float R_STEPDIST = 3.14 * 10 / 200;
 float Z_STEPDIST = 3.14 * 10 / 200;                 //distance pacourue par 1 pas V
-float T_STEP_ANGLE = 1212;
-float T_STEP_ANGLE_ENCODER = 0.45;
-int WORK_ANGLE_TETA = 360;                         //Angle de travail a modifé suivant l'installation de la machine
-int encoderPPR = 800;
-int encoderTicPos = 0;
-int lastEncoderGoal;
+
 int FULL_REVOLUTION_STEPS = 200;
 
-int T_OFFSET = 10;      //Degree
 int R_OFFSET = 150;     //mm
 int Z_MIN_OFFSET = 50; // mm
 int Z_MAX_OFFSET = 50; //mm
@@ -52,42 +42,14 @@ int Z_MAX_OFFSET = 50; //mm
 
 long R_HOMMING_INTERVAL = 1000000; //milliseconds
 long Z_HOMMING_INTERVAL = 1000000;
-long T_HOMMING_INTERVAL = 1000000;
+
 
 
 
 
 #define STEPS 400 // va avec la fonction STEPPER_ACCEL_MOV
 
-void ai0() {
-    // ai0 is activated if DigitalPin nr 2 is going from LOW to HIGH
-    // Check pin 3 to determine the direction
-    if (digitalRead(B_PHASE) == LOW) {
-        counter--;
-    }
-    else {
-        counter++;
-    }
-    if (counter != temp) {
-        //Serial.println(counter);
-        temp = counter;
-    }
-}
 
-void ai1() {
-    // ai0 is activated if DigitalPin nr 3 is going from LOW to HIGH
-    // Check with pin 2 to determine the direction
-    if (digitalRead(A_PHASE) == LOW) {
-        counter++;
-    }
-    else {
-        counter--;
-    }
-    if (counter != temp) {
-        //Serial.println(counter);
-        temp = counter;
-    }
-}
 
 bool timer(long startTime, long interval)
 {
@@ -122,14 +84,7 @@ int CHECK_ENDSTOP_R()
 }
 
 
-int CHECK_ENDSTOP_T()
-{
-    if (digitalRead(TETA_MIN) == HIGH || digitalRead(TETA_MAX) == HIGH)
-    {
-        //send notification error
-        Serial.println("error endswitch sur T");
-    }
-}
+
 
 //***************** POSITION TROLLEY **************************
 int R_POS(int compt, int NEW_POS_R, int minSpeed, int maxSpeed)
@@ -283,147 +238,6 @@ int Z_HOME()
 }
 
 
-//******************** ANGLE POSITON **************************
-int TETA_POS(int compt, int NEW_POS_T, int minSpeed, int maxSpeed)
-{
-
-    int encoderGoal = (NEW_POS_T / T_STEP_ANGLE_ENCODER);
-    /*   int comptToEncoder = compt * (encoderPPR / T_STEP_ANGLE_ENCODER);*/
-       //int numStepsEncoder = ((compt - NEW_POS_T) / T_STEP_ANGLE_ENCODER); //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    float numSTEPS = ((compt - NEW_POS_T) * T_STEP_ANGLE);
-    //encoderTicPos = encoderTicPos + numStepsEncoder;
-
-    if (NEW_POS_T > TETA_MAXSTEP / T_STEP_ANGLE)
-    {
-        (NEW_POS_T) = TETA_MAXSTEP / T_STEP_ANGLE;
-    }
-
-    int lowSpeed = minSpeed;
-    int highSpeed = maxSpeed;
-    int speedDelay = lowSpeed;
-
-
-
-    digitalWrite(TETA_ENABLE, LOW);
-
-    if (encoderGoal < lastEncoderGoal)    // on regarde dans quel sens on part
-    {
-        digitalWrite(TETA_DIR, LOW); // verifier sens ! retour en arriere
-    }
-    else
-    {
-        digitalWrite(TETA_DIR, HIGH); // verifier sens !
-    }
-
-
-    while (counter != encoderGoal)
-    {
-        digitalWrite(TETA_STEP, HIGH);
-        digitalWrite(TETA_STEP, LOW);
-        delayMicroseconds(lowSpeed);
-    }
-    //for (int i = 0; i < abs(numSTEPS); i++)    // on avance ou recule du nombre de pas
-    //{
-    //    if (numStepsEncoder == counter)
-    //    {
-    //        //break;
-    //        Serial.println("je pense etre arrive au point");
-    //    }
-    //    else
-    //    {
-    //        digitalWrite(TETA_STEP, HIGH);
-    //        digitalWrite(TETA_STEP, LOW);
-    //        delayMicroseconds(speedDelay);
-    //        if (i < rampUpEnding)
-    //        {
-    //            speedDelay -= change;
-    //        }
-    //        else if (i > rampDownBegin)
-    //        {
-    //            speedDelay += change;
-    //        }
-    //    }
-    //}
-    //if ()
-    //{
-    //    float delta = counter - numStepsEncoder;
-    //    if (delta + counter > numStepsEncoder)
-    //    {
-    //        digitalWrite(TETA_DIR, HIGH); //check direction
-    //    }
-    //    else
-    //    {
-    //        digitalWrite(TETA_DIR, LOW); //check direction
-    //    }
-    //    for (int i = 0; i < delta; i++)
-    //    {
-    //        digitalWrite(TETA_STEP, HIGH);
-    //        digitalWrite(TETA_STEP, LOW);
-    //        delayMicroseconds(lowSpeed);
-    //    }
-    //}
-    lastEncoderGoal = encoderGoal;
-    digitalWrite(TETA_ENABLE, HIGH);
-    T_COUNT = NEW_POS_T;
-    CHECK_ENDSTOP_T();
-}
-
-//********************** HOME ANGLE ***************************
-int TETA_HOME()
-{
-    T_COUNT = 0;
-    unsigned long startMillis = millis();
-    digitalWrite(TETA_ENABLE, LOW);
-    digitalWrite(TETA_DIR, LOW); // verifier sens !
-    while (digitalRead(TETA_MAX) == LOW)
-    {
-        if (timer(startMillis, T_HOMMING_INTERVAL) == true)
-        {
-            Serial.println("Time out sur T");
-            break;
-        }
-        digitalWrite(TETA_STEP, HIGH);
-        delayMicroseconds(TETA_SPEED);
-        digitalWrite(TETA_STEP, LOW);
-        delayMicroseconds(TETA_SPEED);
-    }
-    digitalWrite(TETA_ENABLE, HIGH);
-    TETA_POS(T_COUNT, T_OFFSET, 2000, 500);
-    T_COUNT = (T_COUNT - T_OFFSET);
-}
-
-//****************** ANGLE CALIBRATION ************************
-int TETA_CALIBRATION()
-{
-    TETA_MAXSTEP = 0;
-    digitalWrite(TETA_ENABLE, LOW);
-    digitalWrite(TETA_DIR, HIGH); // verifier sens !
-    while (digitalRead(TETA_MIN) == LOW)
-    {
-        digitalWrite(TETA_STEP, HIGH);
-        delayMicroseconds(TETA_SPEED);
-        digitalWrite(TETA_STEP, LOW);
-        delayMicroseconds(TETA_SPEED);
-    }
-    delay(50);
-    digitalWrite(TETA_DIR, LOW); // verifier sens !
-    delay(50);
-    while (digitalRead(TETA_MAX) == LOW)
-    {
-        digitalWrite(TETA_STEP, HIGH);
-        delayMicroseconds(TETA_SPEED);
-        digitalWrite(TETA_STEP, LOW);
-        delayMicroseconds(TETA_SPEED);
-        TETA_MAXSTEP = TETA_MAXSTEP + 1;
-    }
-    digitalWrite(TETA_ENABLE, HIGH);
-    T_STEP_ANGLE = ((TETA_MAXSTEP - T_OFFSET) / WORK_ANGLE_TETA);
-    Serial.print("NB pas pour 1 degree: ");
-    Serial.println(T_STEP_ANGLE);
-    Serial.print("Maximum de pas: ");
-    Serial.println(TETA_MAXSTEP);
-}
-
 
 
 
@@ -432,7 +246,6 @@ int HOMMING_ALL()
 {
     Z_HOME();
     R_HOME();
-    TETA_HOME();
 }
 
 
@@ -469,19 +282,6 @@ void STEPPER_ACCEL_MOV(int STEP_PIN) {
 
 
 //****************** ENABLE/DISABLE MOTOR *********************
-void ENABLE_TETA()
-{
-    if (enableStateTETA == true)
-    {
-        digitalWrite(TETA_ENABLE, !enableStateTETA);
-        enableStateTETA = !enableStateTETA;
-    }
-    else
-    {
-        digitalWrite(TETA_ENABLE, !enableStateTETA);
-        enableStateTETA = !enableStateTETA;
-    }
-}
 
 
 void ENABLE_R()
@@ -517,7 +317,6 @@ void ENABLE_Z()
 //**************** ENABLE/DISABLE ALL MOTOR *******************
 void ENABLE_ALL()
 {
-    ENABLE_TETA();
     ENABLE_R();
     ENABLE_Z();
 }
@@ -536,21 +335,7 @@ void SERIAL_COMMAND()
             clrBuffer = Serial.parseInt();
             HOMMING_ALL();
         }
-        else if (Serial.peek() == 'C')
-        {
-            int clrBuffer;
-            TETA_CALIBRATION();
-            Serial.read();
-            clrBuffer = Serial.parseInt();
-        }
-        else if (Serial.peek() == 'T')
-        {
-            float numSteps = 0;
-            Serial.read();
-            numSteps = Serial.parseInt();
-            Serial.println(numSteps);
-            TETA_POS(T_COUNT, numSteps, 2000, 500);
-        }
+
         else if (Serial.peek() == 'R')
         {
             int numSteps = 0;
@@ -568,15 +353,6 @@ void SERIAL_COMMAND()
             Serial.println(numSteps);
             Z_POS(Z_COUNT, numSteps);
         }
-        else if (Serial.peek() == 'P')
-        {
-            int state = 0;
-            Serial.read();
-            state = Serial.parseInt();
-            //Serial.println(state);
-            Serial.println(counter);
-            //digitalWrite(VACUUM_PUMP_ONOFF, state);
-        }
         else if (Serial.peek() == 'E')
         {
             int clrBuffer;
@@ -590,8 +366,6 @@ void SERIAL_COMMAND()
             Serial.read();
             clrBuffer = Serial.parseInt();
         }
-        Serial.print("StepsT : ");
-        Serial.print(T_COUNT);
         Serial.println(" Degree");
         Serial.print("StepsR : ");
         Serial.print(R_COUNT);
@@ -627,12 +401,7 @@ void setup() {
     Z_MIN = 36;         //fin course contact sol
 
 
-    // définition pins rotation bras
-    TETA_ENABLE = 2;    //enable
-    TETA_DIR = 14;      //direction
-    TETA_STEP = 13;     //step
-    TETA_MAX = 34;      //fin de course en haut
-    TETA_MIN = 35;      //fin course contact sol
+
 
     // definition pin auxilaire
     PUMP_ONOFF = 55;    //pompe a eau
@@ -641,7 +410,7 @@ void setup() {
     // delai en micros entre HIGH et LOW 
     Z_SPEED = 500;
     R_SPEED = 500;
-    TETA_SPEED = 500;
+
 
     MAX_R_STEPS = 100000;
     MAX_Z_STEPS = 500000;
@@ -659,11 +428,6 @@ void setup() {
     pinMode(R_STEP, OUTPUT);
     pinMode(R_MIN, INPUT);
 
-    pinMode(TETA_ENABLE, OUTPUT);
-    pinMode(TETA_DIR, OUTPUT);
-    pinMode(TETA_STEP, OUTPUT);
-    pinMode(TETA_MAX, INPUT);
-    pinMode(TETA_MIN, INPUT);
 
     pinMode(PUMP_ONOFF, OUTPUT);
     pinMode(VACUUM_PUMP_ONOFF, OUTPUT);
@@ -671,11 +435,6 @@ void setup() {
     digitalWrite(PUMP_ONOFF, LOW);
 
 
-    pinMode(A_PHASE, INPUT);
-    pinMode(B_PHASE, INPUT);
-    Serial.begin(9600);   //Serial Port Baudrate: 9600
-    attachInterrupt(digitalPinToInterrupt(A_PHASE), ai0, RISING); //Interrupt trigger mode: RISING
-    attachInterrupt(digitalPinToInterrupt(B_PHASE), ai1, RISING); //Interrupt trigger mode: RISING
 
     //TETA_CALIBRATION();
     //HOMMING_ALL();
